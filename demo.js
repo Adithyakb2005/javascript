e317982613b733d4bfcd9e47eb0446edeb3a0085
Adithyakb2005 <adithyakb1001@gmail.com>
2024-09-18 11:21:54 +0530

    javasctipt

---
+++ demo.js
@@ -0,0 +1,39 @@
+// var a
+// a=10
+// let a=10
+// a=20
+// const a=10
+// a=10
+
+// datatype-----
+
+// let a='hai'
+// let a=false
+// let a=null
+// let a={name:'abc'}
+// let a=[1,2,3,4]
+
+// operatorts-----
+
+// let a=10
+// let b=5
+// arithemetic---
+
+// a++
+// a--
+
+// assignment-----
+
+// a+=10
+// a-=10
+// a*=10
+// a/=10
+// a%=10
+
+// comparison---------
+ 
+// let a=10
+// let b='10'
+// alert(a!==b)
+// alert(a===b)
+
