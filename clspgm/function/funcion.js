31fe2b7cb3ecaaebec6ad6c2c865eed9fd654824
Adithyakb2005 <adithyakb1001@gmail.com>
2024-09-21 11:19:11 +0530

    javasctipt

---
+++ clspgm/function/funcion.js
@@ -0,0 +1,20 @@
+// user defined-------->
+function sample(){
+    console.log("userdefined funcion")
+}
+sample()
+// arrow function----------------->
+
+// data=(a,b)=>{
+//     console.log("js arrow function",a,b);
+    
+// }
+// data(10,20)
+
+// nameless funcion ------------->
+
+// data=function(a,b){
+//     console.log("nameless funcion ",a,b);
+      
+// }
+// data(10,20)
---
+++ clspgm/stringmethod/str.js
@@ -0,0 +1,33 @@
+a="Hello world"
+b="      Hell0 Welcome to all       "
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(b.trim(),"ddj");
+console.log(b.trimStart(),"ddj");
+console.log(b.trimEnd(),"ddj");
+console.log(a.charAt(0));
+console.log(a.charCodeAt(0));
+// slice----------
+console.log(a.slice(2,5));
+// match------------>
+console.log(a.match("Hello"));
+console.log(a.match("[a-z].?"));
+console.log(a.match("[a-z].*"));
+console.log(a.match("[a-z].+"));
+console.log(a.match("[a-z].."))
+console.log(b.match("[a-z][0-9]"));
+console.log(b.match("[a-z0-9]"));
+// search--------------->
+console.log(a.search("Hello"));
+
+
+
+
+
+
+
+
+
+
+
