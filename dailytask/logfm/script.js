0ecf759fc6341a71d5f6448a9ac0d3e75f12f07f
Adithyakb2005 <adithyakb1001@gmail.com>
2024-09-23 09:32:01 +0530

    form validation

---
+++ dailytask/logfm/script.js
@@ -0,0 +1,45 @@
+function valid() {
+    
+    document.getElementById("phno_error").innerHTML = "";
+    document.getElementById("email_error").innerHTML = "";
+    document.getElementById("pincode_error").innerHTML = "";
+
+    
+    const name = document.getElementById("name").value;
+    const phno = document.getElementById("phno").value;
+    const email = document.getElementById("email").value;
+    const pincode = document.getElementById("pincode").value; 
+    let isValid = true;
+
+    
+    if (!name) {
+        alert('Please enter your name.');
+        isValid = false;
+    }
+
+    
+    const phonePattern = /^[0-9]{10}$/;
+    if (!phonePattern.test(phno)) {
+        document.getElementById('phno_error').textContent = 'Please enter a valid 10-digit phone number.';
+        isValid = false;
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        document.getElementById('email_error').textContent = 'Please enter a valid Email ID.';
+        isValid = false;
+    }
+
+    
+    const pincodePattern = /^[0-9]{6}$/;
+    if (!pincodePattern.test(pincode)) {
+        document.getElementById('pincode_error').textContent = 'Please enter a six-digit pincode.';
+        isValid = false;
+    }
+
+    
+    if (isValid) {
+        alert('Form submitted successfully!');
+        
+    }
+}
