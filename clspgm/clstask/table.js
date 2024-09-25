0e6a050053e137f76dc2f32def2e0e2b6b2a7e7d
Adithyakb2005 <adithyakb1001@gmail.com>
2024-09-25 11:30:10 +0530

    javasctipt

--- clspgm/clstask/table.js
+++ clspgm/clstask/table.js
@@ -1,6 +1,37 @@
-d=[{name:'anu',age:20,place:'ekm'},{name:'manu',age:30,place:'ekm'},{name:'vinu',age:35,place:'ekm'},{name:'sanu',age:50,place:'ekm'},{name:'ammu',age:22,place:'ekm'},{name:'achu',age:40,place:'ekm'}]
+d=[{name:'anu',age:20,place:'ekm'},{name:'manu',age:30,place:'tvm'},{name:'vinu',age:35,place:'edukki'},{name:'sanu',age:50,place:'kottayam'},{name:'ammu',age:22,place:'kollam'},{name:'achu',age:40,place:'alapuzha'}]
+function display(){
+    const t_var=document.querySelector('#dataTable tbody')
+    t_var.innerHTML=''
+    d.forEach(element => {
+        const t_row=document.createElement('tr')
 
-d.forEach(element => {
-    console.log(element.age);
-    
-});
+        const t_name=document.createElement('td')
+        t_name.textContent=element.name
+        t_row.appendChild(t_name)
+
+        const t_age=document.createElement('td')
+        t_age.textContent=element.age
+        t_row.appendChild(t_age)
+
+        const t_place=document.createElement('td')
+        t_place.textContent=element.place
+        t_row.appendChild(t_place)
+
+        t_var.appendChild(t_row)
+    });
+}
+document.getElementById('submit').addEventListener('submit',function(event){
+    event.preventDefault()  //remove refreshing
+    const name=document.getElementById('name').value
+    const age=parseInt(document.getElementById('age').value)
+    const place=document.getElementById('place').value
+
+    d.push({name:name,age:age,place:place})
+    document.getElementById('name').value=''
+    document.getElementById('age').value=''
+    document.getElementById('place').value=''
+
+    display()
+})
+
+display()
